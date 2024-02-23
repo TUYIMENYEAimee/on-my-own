803aea2f5c5c0ab3ed939f0890fe063fb1ba9f6b
TUYIMENYE <tuyimenyeaimee@gmail.com>
2024-02-23 12:50:56 +0200

    my file is commited well

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my work on devops</title>
+</head>
+<body>
+    <h2>TUYIMENYE Marie Aimee is my name</h2>
+</body>
+</html>
